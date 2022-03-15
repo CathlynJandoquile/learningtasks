184af378e1838e056a2f1db9dd865839928b4ce6
CathlynJandoquile <100071200+CathlynJandoquile@users.noreply.github.com>
2022-03-15 13:45:48 +0800

    Update index.php

--- LT3/index.php
+++ LT3/index.php
@@ -7,7 +7,7 @@
   <h1> Welcome to My Website! I am Jahziel Banne! </h1>
   <ul>
     <li><a href="index.php" target="_blank"> HOME </a></li>
-    <li><a href="/index.php" target= "_blank"> LEARNING TASK 2 </a> </li>
+    <li><a href="/mywebsite.html" target= "_blank"> LEARNING TASK 2 </a> </li>
     <li><a href="/LT3/index.html" target= "_blank"> LEARNING TASK 3 </a> </li>
   </ul>
   </body>
