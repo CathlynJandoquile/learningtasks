0ba2321d9cb81eb39a8a2ee9840731ff63b6067c
CathlynJandoquile <100071200+CathlynJandoquile@users.noreply.github.com>
2022-03-15 13:40:50 +0800

    Add files via upload

--- LT3/index.php
+++ LT3/index.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html>
-     <head>
-       <title>My Home Page</title>
-     </head>
-     <body>
-       <h1> Welcome to my Website i am Cathlyn Jandoquile :)</h1>
-       <ul>
-            <li><a href="index.php">HOME</a></li>
-            <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
-            <li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
-       </ul>
-       </body>
-    
-</html
+  <head>
+    <title> My Home Page </title>
+  </head>
+  <body>
+  <h1> Welcome to My Website! I am Jahziel Banne! </h1>
+  <ul>
+    <li><a href="index.php" target="_blank"> HOME </a></li>
+    <li><a href="/LT2/.HTML" target= "_blank"> LEARNING TASK 2 </a> </li>
+    <li><a href="/LT3/index.html" target= "_blank"> LEARNING TASK 3 </a> </li>
+  </ul>
+  </body>
+</html>
